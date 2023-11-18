d46ac0d14811502f4a11d1dbc55785b71d3c9053
astridplrt <astrid.polart@gmail.com>
2023-11-18 19:21:29 +0100

    automatic slide

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   templateUrl: './landing.html',
   styleUrls: ['./landing.scss']
 })
-export class LandingPage {
+export class LandingPage implements AfterViewInit {
   pictures = [
     'https://ik.imagekit.io/ast/carantec%20logo%20mise%20en%20sc%C3%A8ne1.jpg?updatedAt=1699551705880',
     'https://ik.imagekit.io/ast/Outdoor%20Advertising%20Billboard%20Mockup%20PSD.jpg?updatedAt=1699718546074',
@@ -15,37 +15,45 @@ export class LandingPage {
   ];
   picture = 1;
 
+  @ViewChild('slider')
+  slider!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit(): void {
+    // setInterval(this.slideNext.bind(this), 3000);  // l'un ou l'autre, car perd le "this" de slideNext (this.slider)
+    setInterval(() => this.slideNext(), 3000);
+  }
+
   slideNext() {
-    const slider = document.getElementById('slider');
-    slider!.style.transition = "all ease-in-out 300ms";
-    slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture})))`;
+    const slider = this.slider.nativeElement;
+    slider.style.transition = "all ease-in-out 300ms";
+    slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture})))`;
     
     if (this.picture < 5) {
       this.picture++;
     } else {
       setTimeout(() => {
-        slider!.style.transition = "none";
+        slider.style.transition = "none";
         this.picture = 1;
-        slider!.style.transform = `translateX(0)`;
+        slider.style.transform = `translateX(0)`;
       }, 400);
     }
   }
   
   slidePrevious() {
-    const slider = document.getElementById('slider');
+    const slider = this.slider.nativeElement; 
     
     if (this.picture > 1) {
       this.picture--;
-      slider!.style.transition = "all ease-in-out 300ms";
-      slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
+      slider.style.transition = "all ease-in-out 300ms";
+      slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
     } else {
       this.picture = 6;
-      slider!.style.transition = "none";
-      slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
+      slider.style.transition = "none";
+      slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
       setTimeout(() => {
         this.picture--;
-        slider!.style.transition = "all ease-in-out 300ms";
-        slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
+        slider.style.transition = "all ease-in-out 300ms";
+        slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
       }, 50);
     }
   }
