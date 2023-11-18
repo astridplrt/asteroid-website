687f6c79e8086131f75e2e7fdf07176d40fdfa4f
astridplrt <astrid.polart@gmail.com>
2023-11-18 18:58:54 +0100

    Carousel with buttons and loop

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,9 +1,52 @@
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './landing.html',
-    styleUrls: ['./landing.scss']
+  templateUrl: './landing.html',
+  styleUrls: ['./landing.scss']
 })
 export class LandingPage {
+  pictures = [
+    'https://ik.imagekit.io/ast/carantec%20logo%20mise%20en%20sc%C3%A8ne1.jpg?updatedAt=1699551705880',
+    'https://ik.imagekit.io/ast/Outdoor%20Advertising%20Billboard%20Mockup%20PSD.jpg?updatedAt=1699718546074',
+    'https://ik.imagekit.io/ast/logo%20m%C3%A9tal.jpg?updatedAt=1699551693444',
+    'https://ik.imagekit.io/ast/old%20door%20in%20the%20forest.jpg?updatedAt=1699718532935',
+    'https://ik.imagekit.io/ast/volage%20lingerie%20mockup.png?updatedAt=1699718566043',
+    'https://ik.imagekit.io/ast/carantec%20logo%20mise%20en%20sc%C3%A8ne1.jpg?updatedAt=1699551705880'
+  ];
+  picture = 1;
+
+  slideNext() {
+    const slider = document.getElementById('slider');
+    slider!.style.transition = "all ease-in-out 300ms";
+    slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture})))`;
+    
+    if (this.picture < 5) {
+      this.picture++;
+    } else {
+      setTimeout(() => {
+        slider!.style.transition = "none";
+        this.picture = 1;
+        slider!.style.transform = `translateX(0)`;
+      }, 400);
+    }
+  }
+  
+  slidePrevious() {
+    const slider = document.getElementById('slider');
+    
+    if (this.picture > 1) {
+      this.picture--;
+      slider!.style.transition = "all ease-in-out 300ms";
+      slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
+    } else {
+      this.picture = 6;
+      slider!.style.transition = "none";
+      slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
+      setTimeout(() => {
+        this.picture--;
+        slider!.style.transition = "all ease-in-out 300ms";
+        slider!.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
+      }, 50);
+    }
+  }
 }
