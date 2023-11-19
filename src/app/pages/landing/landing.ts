40d2ee8333d89037e73da9e39aceee8fc9be28ad
astridplrt <astrid.polart@gmail.com>
2023-11-19 12:01:11 +0100

    carousel : reset timer on click

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -18,21 +18,35 @@ export class LandingPage implements AfterViewInit {
   @ViewChild('slider')
   slider!: ElementRef<HTMLDivElement>;
 
+  timer: number | null = null;
+
   ngAfterViewInit(): void {
-    // setInterval(this.slideNext.bind(this), 3000);  // l'un ou l'autre, car perd le "this" de slideNext (this.slider)
-    setInterval(() => this.slideNext(), 3000);
+    this.startTimer();
+  }
+  
+  private startTimer() {
+    // this.timer = setInterval(this.slideNext.bind(this), 3000);  // l'un ou l'autre, car perd le "this" de slideNext (this.slider)
+    this.timer = window.setInterval(() => this.slideNext(), 3000);
   }
 
+  private resetTimer() {
+    if (this.timer != null) {
+      window.clearInterval(this.timer);
+      this.startTimer();
+    }
+  }
+  
   slideNext() {
+    this.resetTimer();
     const slider = this.slider.nativeElement;
-    slider.style.transition = "all ease-in-out 300ms";
+    slider.style.transition = 'all ease-in-out 300ms';
     slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture})))`;
-    
+
     if (this.picture < 5) {
       this.picture++;
     } else {
       setTimeout(() => {
-        slider.style.transition = "none";
+        slider.style.transition = 'none';
         this.picture = 1;
         slider.style.transform = `translateX(0)`;
       }, 400);
@@ -40,19 +54,20 @@ export class LandingPage implements AfterViewInit {
   }
   
   slidePrevious() {
-    const slider = this.slider.nativeElement; 
-    
+    this.resetTimer();
+    const slider = this.slider.nativeElement;
+
     if (this.picture > 1) {
       this.picture--;
-      slider.style.transition = "all ease-in-out 300ms";
+      slider.style.transition = 'all ease-in-out 300ms';
       slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
     } else {
       this.picture = 6;
-      slider.style.transition = "none";
+      slider.style.transition = 'none';
       slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
       setTimeout(() => {
         this.picture--;
-        slider.style.transition = "all ease-in-out 300ms";
+        slider.style.transition = 'all ease-in-out 300ms';
         slider.style.transform = `translateX(calc(-100%/${this.pictures.length} * (${this.picture} - 1)))`;
       }, 50);
     }
