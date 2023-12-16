0b6598558fd728f252b3c15890e75f982aaa700b
astridplrt <astrid.polart@gmail.com>
2023-12-16 10:55:36 +0100

    Fixes header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AboutPage } from "./pages/about/about";
-import { ContactPage } from "./pages/contact/contact";
-import { LandingPage } from "./pages/landing/landing";
+import { AboutPage } from './pages/about/about';
+import { ContactPage } from './pages/contact/contact';
+import { LandingPage } from './pages/landing/landing';
 import { PortfolioPage } from './pages/portfolio/portfolio';
 import { AmbrussumPage } from './pages/portfolio-detail/ambrussum';
 import { AnfaPage } from './pages/portfolio-detail/anfa';
@@ -19,91 +19,87 @@ import { OpenworkersPage } from './pages/portfolio-detail/openworkers';
 import { RetroLegendPage } from './pages/portfolio-detail/retrolegend';
 import { SpiAsymetriquePage } from './pages/portfolio-detail/spiasymetrique';
 
-@NgModule(
-  { exports: [        
-      RouterModule
-  ],
-    imports: [
-      RouterModule.forRoot([
-        {
-          path: 'about',
-          component: AboutPage
-        },
-        {
-          path: 'contact',
-          component: ContactPage
-        },
-        {
-          path: 'landing',
-          component: LandingPage
-        },
-        {
-            path: 'portfolio',
-            component: PortfolioPage
-        },
-        {
-            path: 'ambrussum',
-            component: AmbrussumPage
-        },
-        {
-            path: 'anfa',
-            component: AnfaPage
-        },
-        {
-            path: 'ats3',
-            component: ATS3Page
-        },
-        {
-            path: 'avoine',
-            component: AvoinePage
-        },
-        {
-            path: 'breizhbrico',
-            component: BreizhBricoPage
-        },
-        {
-            path: 'carantec',
-            component: CarantecPage
-        },
-        {
-            path: 'devcat',
-            component: DevcatPage
-        },
-        {
-            path: 'directimmo',
-            component: DirectImmoPage
-        },
-        {
-            path: 'forcewest',
-            component: ForceWestPage
-        },
-        {
-            path: 'impressionschinoises',
-            component: ImpressionsChinoisesPage
-        },
-        {
-            path: 'laille',
-            component: LaillePage
-        },
-        {
-            path: 'openworkers',
-            component: OpenworkersPage
-        },
-        {
-            path: 'retrolegend',
-            component: RetroLegendPage
-        },
-        {
-            path: 'spiasymetrique',
-            component: SpiAsymetriquePage
-        },
-        {
-            path: '**',
-            redirectTo: 'landing'
-        }
-      ])
-
-    ]
-  }
-)
-export class AppRoutingModule { }
+@NgModule({
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot([
+      {
+        path: 'about',
+        component: AboutPage
+      },
+      {
+        path: 'contact',
+        component: ContactPage
+      },
+      {
+        path: 'landing',
+        component: LandingPage
+      },
+      {
+        path: 'portfolio',
+        component: PortfolioPage
+      },
+      {
+        path: 'ambrussum',
+        component: AmbrussumPage
+      },
+      {
+        path: 'anfa',
+        component: AnfaPage
+      },
+      {
+        path: 'ats3',
+        component: ATS3Page
+      },
+      {
+        path: 'avoine',
+        component: AvoinePage
+      },
+      {
+        path: 'breizhbrico',
+        component: BreizhBricoPage
+      },
+      {
+        path: 'carantec',
+        component: CarantecPage
+      },
+      {
+        path: 'devcat',
+        component: DevcatPage
+      },
+      {
+        path: 'directimmo',
+        component: DirectImmoPage
+      },
+      {
+        path: 'forcewest',
+        component: ForceWestPage
+      },
+      {
+        path: 'impressionschinoises',
+        component: ImpressionsChinoisesPage
+      },
+      {
+        path: 'laille',
+        component: LaillePage
+      },
+      {
+        path: 'openworkers',
+        component: OpenworkersPage
+      },
+      {
+        path: 'retrolegend',
+        component: RetroLegendPage
+      },
+      {
+        path: 'spiasymetrique',
+        component: SpiAsymetriquePage
+      },
+      {
+        path: '**',
+        redirectTo: 'landing'
+      }
+    ])
+  ]
+})
+export class AppRoutingModule {}
